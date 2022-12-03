d04f067211244d09b1627abc47f2d8a93616063d
Tung Beier <beiertu@mediamarktsaturn.com>
2022-12-03 18:07:26 +0100

    feat: Change default branch name in combine-prs

--- combine-prs.sh
+++ combine-prs.sh
@@ -14,7 +14,7 @@ usage: ./combine-prs.sh [OPTIONS]
 
 Options:
 -b  default head branch to be checkout from. Default: master.
--c  feature branch name to be used. Default: Update-dependencies.
+-c  feature branch name to be used. Default: build_Update-repo-dependencies.
 -s  earch criteria for branches to be considered in the combination. Default: 'dependabot-'.
 *   show this usage.
 EOF
@@ -29,7 +29,7 @@ function run() {
 
 	# Parse arguments
 	local base_branch="master"
-	local combine_branch_name="Update-repo-dependencies"
+	local combine_branch_name="build_Update-repo-dependencies"
 	local search_branch_name="dependabot-"
 
 	while getopts ":b:c:s:" option; do
